bd96aa153678786d9c292d169eecb914255b38f2
Corina Andariese <candariese@gmail.com>
2018-08-08 16:57:58 -0700

    added new input feild and button, change handlers, and submit handlers

--- src/components/CardComponents/card_form.jsx
+++ src/components/CardComponents/card_form.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../../node_modules/bulma/bulma.sass';
 
-const CardForm = () => {
-	return (
-		<div className="field">
-			<div className="control">
-				<input
-					className="input is-primary"
-					type="text"
-					placeholder="Primary input"
-				/>
-			</div>
-		</div>
-	);
-};
+class CardForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: '' };
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	handleSubmit(event) {
+		console.log(`A word or phrase was submitted!: ${this.state.value}`);
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<form
+				className="field"
+				onSubmit={(event) => {
+					this.handleSubmit(event);
+				}}>
+				<div className="control">
+					<input
+						value={this.state.value}
+						onChange={(event) => this.handleChange(event)}
+						className="input is-primary"
+						type="text"
+						data-parser="uppercase"
+						placeholder="Enter English word or phrase here"
+					/>
+					<br />
+					<br />
+					<button type="submit" value="Submit" className="button is-primary">
+						Translate
+					</button>
+				</div>
+			</form>
+		);
+	}
+}
 
 export default CardForm;
