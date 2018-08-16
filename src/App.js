280e1b8ef771ec37ba5da695fa490f60aa67b96d
Corina Andariese <candariese@gmail.com>
2018-08-16 13:04:17 -0700

    Added card and card flip animation for later use

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import './sass/main.scss';
 import CardForm from './components/CardComponents/CardForm.jsx';
 import NavBar from './components/NavBar/Nav_Bar.jsx';
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<NavBar />
-				<CardForm />
-			</div>
-		);
-	}
-}
+import FlashCard from './components/CardComponents/FlashCard.jsx';
+
+const App = () => {
+	const styles = {
+		card: {
+			border: '.5px solid black',
+			borderRadius: '3px',
+			padding: '15px',
+			width: '250px'
+		},
+		image: {
+			height: '200px',
+			width: '250px'
+		}
+	};
+	return (
+		<div className="App">
+			<NavBar />
+			<CardForm />
+			<FlashCard styles={styles} />
+		</div>
+	);
+};
 
 export default App;
