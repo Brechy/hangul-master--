d4f4b21ed20e5fc3a98a9fad22f60ed2d759a7ca
Corina Andariese <candariese@gmail.com>
2018-08-16 09:46:40 -0700

    Included navbar to main app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './sass/main.scss';
 import CardForm from './components/CardComponents/card_form.jsx';
+import NavBar from './components/NavBar/Nav_Bar.jsx';
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<NavBar />
 				<CardForm />
 			</div>
 		);
