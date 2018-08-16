2c0a46066bb8981c0dd143b3b865e7546d2a2ea5
Corina Andariese <candariese@gmail.com>
2018-08-16 12:20:52 -0700

    Refectored name of CardForm component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './sass/main.scss';
-import CardForm from './components/CardComponents/card_form.jsx';
+import CardForm from './components/CardComponents/CardForm.jsx';
 import NavBar from './components/NavBar/Nav_Bar.jsx';
 class App extends Component {
 	render() {
