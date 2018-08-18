a4abe86051e1506624015f427f1a003cfd77e16e
Corina Andariese <candariese@gmail.com>
2018-08-17 19:07:31 -0700

    Created route for dashboard page

--- routes.jsx
+++ routes.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import LandingPage from './src/components/LandingPage/LandingPage.jsx';
-import LoginPage from './src/components/Login/LoginForm.jsx';
+import LoginPage from './src/components/Login/LoginPage.jsx';
+import DashboardPage from './src/components/UserDashboard/DashboardPage.jsx';
 
 const Routes = () => (
 	<Switch>
 		<Route exact path="/" component={LandingPage} />
 		<Route path="/LoginPage" component={LoginPage} />
+		<Route path="/DashboardPage" component={DashboardPage} />
 		<Route path="*" render={() => <Redirect to="/" />} />
 	</Switch>
 );
