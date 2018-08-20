f86813371e6f25271570338108f77ef76a91aacc
Corina Andariese <candariese@gmail.com>
2018-08-19 17:49:57 -0700

    Added jwt auth, saved token to local storage on successful login

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GoogleLogin } from 'react-google-login';
 
-const responseGoogle = (response) => {
+const onFailure = (response) => {
 	console.log(response);
-};
+}
+
+const onSuccess = (response) => {
+	console.log(JSON.stringify(response))
+		console.log(response)
+
+	localStorage.setItem('jwt token', response.tokenId)
+}
 
 const LoginForm = () => {
 	return (
 		<GoogleLogin
 			clientId="985262096986-ebd9c6615ps9map91sc7gmjhrenuf1t1.apps.googleusercontent.com"
 			buttonText="Sign in"
-			onSuccess={responseGoogle}
-			onFailure={responseGoogle}
+			onSuccess={onSuccess}
+			onFailure={onFailure}
 		/>
 	);
 };
