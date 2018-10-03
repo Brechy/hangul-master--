86f26f2b4e3d0f2e9c58958f20446efd9729c8cb
Brechin <brechin@brechy.net>
2018-10-03 10:56:06 -0700

    updated comments

--- src/components/Authenticated/Authenticated.jsx
+++ src/components/Authenticated/Authenticated.jsx
@@ -1,17 +1,7 @@
-//visit a Page
-
-//is user logged in?
-//YES -> Does User have a Username? -> YES -> Go to the page visited (logged in as that user)
-//NO -> Does User have Userame? -> YES -> Go to the Page
-//NO -> Does User have Username? -> NO -> send to Username Selection page
-
-//All pages reset and start from the beginning flow above, is user logged in?
-
 import React from 'react';
 
 const Authenticate = () => {
   return (
-
 
         <meta name="google-signin-scope" content="profile email">
           <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
@@ -20,12 +10,13 @@ const Authenticate = () => {
               <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
               <script>
                 function onSignIn(googleUser) {
-                  // The ID token you need to pass to your backend:
+                  //ID token to pass to backend:
                   const id_token = googleUser.getAuthResponse().id_token;
                   console.log("ID Token: " + id_token);
                 };
               </script>
             </body>
 
-  )
-}
+            )
+            }
+            
