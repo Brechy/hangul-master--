a94fd1df862238d5a3210a9666fc6336f869f633
Brechin <brechin@brechy.net>
2018-10-05 11:12:15 -0700

    add signup form to landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import SignupForm from '../Signup/SignupForm.jsx'
 import '../../../node_modules/bulma/bulma.sass';
 
 const LandingPage = () => {
-	return <div id="LandingPage" />;
+	return <div id="LandingPage">
+		<SignupForm />
+	</div>
 };
 
 export default LandingPage;
