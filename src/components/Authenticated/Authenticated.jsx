dff7d8b70a9c681b18fe50f1523acc2427044838
Brechin <brechin@brechy.net>
2018-10-06 15:02:14 -0700

    refactored auth component

--- src/components/Authenticated/Authenticated.jsx
+++ src/components/Authenticated/Authenticated.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 
 const Authenticate = () => {
-  return (
+  return (<meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
+      <script src="https://apis.google.com/js/platform.js" async="async" defer="defer"></script>
+      <body>
+        <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+        <script>
+          function onSignIn(googleUser) {
+            //ID token to pass to backend:
+            const id_token = googleUser.getAuthResponse().id_token;
+            console.log("ID Token: " + id_token);
+          }
+        </script>
+      </body>
 
-        <meta name="google-signin-scope" content="profile email">
-          <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
-            <script src="https://apis.google.com/js/platform.js" async defer></script>
-            <body>
-              <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-              <script>
-                function onSignIn(googleUser) {
-                  //ID token to pass to backend:
-                  const id_token = googleUser.getAuthResponse().id_token;
-                  console.log("ID Token: " + id_token);
-                };
-              </script>
-            </body>
-
-            )
-            }
-            
+      )}
